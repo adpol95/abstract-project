61c652a2dbf96f24c86cbef521a18036c9d0ab58
adpol <adpol95@gmail.com>
2022-11-22 18:01:01 +0300

    Доработана основная секция(поиска) главной страницы

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
 import './App.css';
 
+
 function App() {
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div className="App">
             <header>
-                <div id="leftSection">
-                    <div id="logoHead">
+                <div className="leftSection">
+                    <div className="logoHead">
                         <img
                             src="https://theme.zdassets.com/theme_assets/2197739/df1f3a3595ad6f2e809942c0cbfa85f84156dd61.svg"
                             alt="logo"/>
                     </div>
-                    <div id="textLogoHeadBlock">
-                        <p id="dividerLogoWText">
-                            I
-                        </p>
-                        <a href="https://help.abstract.com/hc/en-us" id="textLogoHead">
+                    <div className="dividerLogoWText">
+                        I
+                    </div>
+                    <div className="textLogoHeadBlock">
+                        <a href="https://help.abstract.com/hc/en-us" className="textLogoHead">
                             Help Center
                         </a>
                     </div>
                 </div>
 
-                <div id="rightSection">
-                    <div id="blockLeftBut">
-                        <button id="leftBut">
+                <div className="rightSection">
+                    <div className="blockLeftBut">
+                        <button className="leftBut">
                             Submit a request
                         </button>
                     </div>
-                    <div id="blockRightBut">
-                        <button id="rightBut">
+                    <div className="blockRightBut">
+                        <button className="rightBut">
                             Sign in
                         </button>
                     </div>
                 </div>
             </header>
+
+            <main>
+                <section className="searchingArea">
+                    <div className="descSearchText">
+                        How can we help?
+                    </div>
+                    <div className="mainSearchingBar">
+                        <input type="text" placeholder="Search"/>
+                        <img alt="arrow-right" className="myClass1"/>
+                    </div>
+                </section>
+
+            </main>
+
         </div>
     );
 }
